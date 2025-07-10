70eb2b12df9a7be55ea54f420349ae8551ee57c7
VANESSARMIREZ <vanessa.gomez2859@alumnos.udg.mx>
2025-07-09 23:04:18 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'localhost';
-$db = 'musicpower';
-$user = 'postgres';
-$pass = 'V02032003';
+$host = 'dpg-d1nkheadbo4c73en0ne0-a.oregon-postgres.render.com';
 $port = '5432';
+$dbname = 'musicpower_r2rc';
+$user = 'girlpower';
+$password = 'Vc6AuwLsOHB6Zasc86mHF3NM1RqUKXps';
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error al conectar con la base de datos: " . $e->getMessage());
